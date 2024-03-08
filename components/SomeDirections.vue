658fdba32fe026050a934d706a922c54da71776d
cfdxkk <b2567240058@gmail.com>
2024-03-08 11:53:01 +0800

    :memo: Fix typo

--- components/SomeDirections.vue
+++ components/SomeDirections.vue
@@ -1,6 +1,7 @@
 <template>
 	<div>
-		<p>Image one is a default HTML img tag, image two is an NuxtImg tag with assess image file in /public, image three is an NuxtImg tag with image file from internet.</p>
+		<p>This demo is running in Vercel now.</p>
+		<p>Image one is a default HTML img tag, image two is an NuxtImg tag with static image file in /public, image three is an NuxtImg tag with image file from internet.</p>
 		<p>And my <code>nuxt.config.ts</code> file is like this, I use ipx for provider in both local development environment and Vercel production environment.</p>
 		<pre>
 			<code>
